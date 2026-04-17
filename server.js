b7e9db23335471fd9d19a0d3f106dfc377caa8c7
Fareed Hussain <yourEmail@gmail.com>
2026-04-17 09:49:40 +0500

    clean start

---
+++ server.js
@@ -0,0 +1,56 @@
+import express, { json } from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose, { connect } from "mongoose";
+import chatRoutes from "./routes/chat.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", chatRoutes)
+app.use("/api/auth", authRoutes)
+
+const connectDB = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("connected with DataBase!")
+  }catch(err){
+    console.log("Failed to connect with Db", err)
+  }
+};
+
+// app.post("/test", async(req,res)=>{
+
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+//     },
+//     body: JSON.stringify({
+//       model: "llama-3.1-8b-instant",
+//       messages: [{
+//         role: "user",
+//         content: req.body.message
+//       }]
+//     })
+//   }
+
+//   try{
+//     const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options)
+//     const data = await response.json();
+//     console.log(data.choices[0].message.content)
+//     res.send(data);
+//   }catch(err){
+//     console.log(err)
+//   }
+// });
+
+app.listen(PORT, ()=>{
+  console.log(`server running on ${PORT}`)
+  connectDB();
+});
